c3ac385c9d2e35a63efa1d97eec03585e335cd45
VladRidun <ridunvv@gmail.com>
2024-01-21 10:22:02 +0400

    4. TODO list [удалил  liquibase_test]

--- src/main/java/ru/job4j/todo/model/Task.java
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -2,9 +2,7 @@ package ru.job4j.todo.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 
-import javax.persistence.Entity;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -17,12 +15,8 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "title")
     private String title;
-    @Column(name = "description")
     private String description;
-    @Column(name = "created")
     private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-    @Column(name = "done")
     private boolean done;
 }
