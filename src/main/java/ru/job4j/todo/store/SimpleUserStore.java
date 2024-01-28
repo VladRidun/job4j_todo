c928a0f8cc0be68afa3bdfd3cc9f5b98d29eb7e4
VladRidun <ridunvv@gmail.com>
2024-01-28 11:19:04 +0400

    4. TODO list [add try-catch]

--- src/main/java/ru/job4j/todo/store/SimpleUserStore.java
+++ src/main/java/ru/job4j/todo/store/SimpleUserStore.java
@@ -1,10 +1,7 @@
 package ru.job4j.todo.store;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
-import ru.job4j.todo.model.Task;
 import ru.job4j.todo.model.User;
 
 import java.util.Map;
@@ -23,9 +20,14 @@ public class SimpleUserStore implements UserStore {
 
     @Override
     public Optional<User> findByLoginAndPassword(String login, String password) {
-        return crudRepository.optional(
-                "FROM User WHERE login = :fLogin AND password = :fPassword", User.class,
-                Map.of("fLogin", login, "fPassword", password)
-        );
+        try {
+            return  crudRepository.optional(
+                    "FROM User WHERE login = :fLogin AND password = :fPassword", User.class,
+                    Map.of("fLogin", login, "fPassword", password)
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
     }
 }
